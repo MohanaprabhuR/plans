368ab231583f1186d042abdad74eaaec2130867b
Mohanaprabhu.R <mohan@timeless.co>
2025-08-26 09:42:37 +0530

    Contact page and vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 // vite.config.ts
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import legacy from "@vitejs/plugin-legacy";
+import { splitVendorChunkPlugin } from "vite";
+
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), legacy(), splitVendorChunkPlugin()],
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        contact: path.resolve(__dirname, "contact.html"),
+      },
+    },
+  },
 });
